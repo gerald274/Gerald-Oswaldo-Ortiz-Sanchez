0848b0124014e6595bc49b14556a792adaa6f54b
gerald274 <dariolasanchezaguilar@gmail.com>
2024-08-25 16:22:28 -0600

    primer commit

---
+++ 01_Repaso.js
@@ -0,0 +1,8 @@
+let nombre= "Gerald";
+var apellidos= "Sanchez";
+const edad=30;
+
+console.log("Mi nombre es" ${nombre} ${apellidos} y tengo  ${edad})
+
+edad=25;
+nombre="jose";
---
+++ 02_repaso.js
@@ -0,0 +1,10 @@
+let arreglo2D =[];
+
+let arreglo1= [1,2,3,4];
+let arreglo2= ['a','b','c','d'];
+
+function lectura(){
+    for (let i=0;i<arreglo1.length;i++){
+        let valores[arreglo1]
+    }
+}
