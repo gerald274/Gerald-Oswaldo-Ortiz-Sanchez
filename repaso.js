a2f424365f8e04a8c3e6f6d428d4914872a15433
gerald274 <dariolasanchezaguilar@gmail.com>
2024-09-02 11:36:01 -0600

    correcciones

--- repaso.js
+++ repaso.js
@@ -1,10 +1,25 @@
+
+// Definición del objeto perrito
 const perrito = {
-    nombre: "gerald",
-    edad: 19,
-    sexo: "m"
+    nombre: null,
+    edad: null,
+    sexo: null
 };
+
+// Asignación de un valor a la propiedad nombre usando la notación de punto
+perrito.nombre = "gerald";
 console.log(perrito);
 
-// Desestructuración de propiedades
+// Desestructuración de un arreglo
+const datos = [1, "hola", 23];
+let [num1, cambrano, num3] = datos;
+
+// Uso del spread operator para crear una copia del objeto perrito
+let copiaDatos = { ...perrito };
+copiaDatos.nombre = "juan";
+console.log(copiaDatos.edad);
+
+// Desestructuración de propiedades del objeto perrito
 let { nombre, edad, sexo } = perrito;
 console.log(nombre, sexo);
+
